48dd3bb35461f16da6fa81edd91e20f1b6b58939
Shivani Saini <52560286+shivani3495@users.noreply.github.com>
2021-03-01 18:10:00 +0530

    Update state.tf

--- tfdjango/state.tf
+++ tfdjango/state.tf
@@ -5,8 +5,7 @@ terraform {
         encrypt = true
         key = "terraform.tfstate"
         region = "ap-south-1"
-        access_key = "AKIAJREE64MWXF5C7CFQ"
-        secret_key = "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"
+       
         
         
     }
