995c942150a4d7761e1aa02183f6c87f14c4a480
Shivani Saini <52560286+shivani3495@users.noreply.github.com>
2021-12-16 20:16:29 +0530

    Update state.tf

--- state.tf
+++ state.tf
@@ -5,6 +5,8 @@ terraform {
         encrypt = true
         key = "terraform.tfstate"
         region = "ap-south-1"
+        access_key = "AKIAWOUNPWBWPVXSLSO6"
+        secret_key = "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"
         
         
     }
