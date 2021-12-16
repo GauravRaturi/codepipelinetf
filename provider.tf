3fbbd72aea650267304be957d7374734d9ac7c2d
Shivani Saini <52560286+shivani3495@users.noreply.github.com>
2021-12-16 21:16:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 provider "aws"{
-    region : "ap-south-1"
-    access_key : ${{ secrets.AWS_ACCESS_KEY_ID }}
-    secret_key : ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+    region = "ap-south-1"
+    access_key = "AKIAWOUNPWBWPVXSLSO6"
+    secret_key = "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"
+        
 }
