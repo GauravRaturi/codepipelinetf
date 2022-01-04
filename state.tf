92d7a847e3db84927b1488003954d67aa748c8f9
Shivani Saini <52560286+shivani3495@users.noreply.github.com>
2022-01-04 12:46:08 +0530

    Update state.tf

--- state.tf
+++ state.tf
@@ -1,16 +1,4 @@
 
-terraform {
-    backend "s3" {
-        bucket = "mypipelinecode"
-        encrypt = true
-        key = "terraform.tfstate"
-        region = "ap-south-1"
-        access_key = "AKIAWOUNPWBWPVXSLSO6"
-        secret_key = "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"
-        
-        
-    }
-}
 
 
 
